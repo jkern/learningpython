8aa0e9ee995e4e5b3b382845a930fcbb16f68c6b
jkern <jkern@vanguard.localdomain>
2008-10-07 11:50:22 -0400

    Initial Commit

---
+++ 1.Hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+# 1.Hello.py -- Demonstrating a single line print statement
+
+print "Hello World!"
---
+++ 2.Hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# 1.Hello.py -- Demonstrating a multi line print statement
+
+print """Hello,
+World!"""
