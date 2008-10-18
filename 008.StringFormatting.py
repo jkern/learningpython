26da4f464323782192c1da90ce4773846c3a6b15
jkern <joseph.a.kern@gmail.com>
2008-10-18 10:03:38 -0700

    Spelling

--- 008.StringFormatting.py
+++ 008.StringFormatting.py
@@ -2,7 +2,7 @@
 
 # -- An introduction to string formatting
 
-print "\n Okay enough with the Hello World! already.\n We need somthing different!\n" 
+print "\n Okay enough with the Hello World! already.\n We need something different!\n" 
 		# Notice how the '\n' does not print? It is actually a newline statement.
 		# There are other types of statements like this.
 		# \n = Newline
