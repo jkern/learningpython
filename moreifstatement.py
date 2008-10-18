0e8bdc5fa76528efce4b8e31acdf551997052b35
jak <joseph.a.kern@gmail.com>
2008-10-18 13:16:33 -0400

    Added and changed the if loop examples

--- moreifstatement.py
+++ moreifstatement.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-# -- More if statments, this is where it gets fun!
-
+# -- More if statements, this is where it gets fun!
+# Try different values for y
 y = 3
 
-if y == 3:
+if y == 3:	# this does NOT make y = 3 it checks to see if y is equivalent to 3, y == 3
 	print "Y is equal to 3!"
 else:
 	print "Y is NOT equal to 3!"
 
 if (y > 0) and (y < 10):
 	print "Y is greater than 0 and less than 10!"
-elif y =< 0:
-	print "Y is 0 or less!"
+elif y >= 10:
+	print "Y is 10 or more!"
+else:
+	print "Y is less than 0!"
 
