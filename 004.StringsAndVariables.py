faa97eb2898b4fa1dbd675579c38894001876f23
jak <joseph.a.kern@gmail.com>
2008-10-29 20:18:35 -0400

    Added 012 raw user input

--- 004.StringsAndVariables.py
+++ 004.StringsAndVariables.py
@@ -11,5 +11,7 @@ w = "World!" 	# Another string
 
 print h,w 	# We must print variable seperatly the comma allows us to seperate them
 		# Try it without it.
+		# If we add a comma as the last item the next line that prints will be right next to the previous one.
+			# print h,w,
 
 		# A variable may contain any number of Objects; Strings, Numbers, Lists, Dictionaries, Tuples, and even Files!
---
+++ 012.RawUserInput.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+This demonstrates user input via the raw_input() function.
+"""
+
+# Let's make a variable to hold the input fromt the user.
+input_string = raw_input("\n\nPlease enter some text.")
+# Here is one of the cool capabilities of python.
+# We can store the input BEFORE its actually avalible.
+
+print input_string
+
+
