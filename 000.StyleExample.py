b83eee88232fce56d0812bf66a180919a5e754b0
jak <joseph.a.kern@gmail.com>
2008-11-03 17:15:45 -0500

    Added a new file to help document style.

---
+++ 000.StyleExample.py
@@ -0,0 +1,24 @@
+#!/usr/bn/python
+""" The docstring goes here. The first line is a brief description of your program.
+
+Long description (if applicable).
+
+FirstName LastName
+WIS290
+FALL 2008 Block II
+"""
+
+import strings # Import statements should be ONE to a line
+import os
+
+
+# Comments should be on their own line ABOVE the line you are commenting on.
+VariableNames = "Should be in a CamelCase format."
+VariableNames2 = "Should also be descriptive of their values or funtions."
+
+def FunctionOne():
+	"""Doc strings should also be used to document functions.
+
+	And should also be in the same format as the main docstring.
+	"""
+
