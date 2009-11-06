f2767322934d7aea970cb40e4dbf363060b10179
jkern <joseph.a.kern@gmail.com>
2009-11-06 08:14:40 -0500

    Updated the README.

--- 012a.LambdaFunctions.py
+++ 012a.LambdaFunctions.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
-""" Functions can be named (as in the previous example) or unamed. An unnamed function is called a lambda. At first it may seem odd that we need to name an un-named function. Think of lambdas as a noun, rather than a proper noun. A generic function. """
+""" Functions can be named (as in the previous example) or unnamed. An unnamed function is called a lambda. At first it may seem odd that we need to name an un-named function. Think of lambdas as a noun, rather than a proper noun. A generic function. """
 
 # Getting started let's create a simple squaring function
 
 square = lambda x: x * x
 
-# So we've created a sqauring function, that will take the input x and return the sqaure of x.
-# the function lambda, creates a function without a def statement. This is the begining of functional programming.
+# So we've created a squaring function, that will take the input x and return the square of x.
+# the function lambda, creates a function without a def statement. This is the beginning of functional programming.
 
 # Functions can be created in this manner, and like all functions can be used as the input for other functions.
 
-print "This is the sqaure of 12 : %d" % square(12)
-print "This is the square of the sqaure of 12 : %d" % square(square(12))
+print "This is the square of 12 : %d" % square(12)
+print "This is the square of the square of 12 : %d" % square(square(12))
 
 # lambdas are limited by design in python, but there will be certain times they are useful. You will know when the time comes.
 
