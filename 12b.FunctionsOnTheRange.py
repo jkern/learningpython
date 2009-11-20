c0c5f8a2d72d769d758d11a37af58138de51e765
Joseph Kern <jkern@happy.localdomain>
2009-11-19 23:12:15 -0500

    Moved 12c and 12b to 012b 012c

--- 12b.FunctionsOnTheRange.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-"""A brief example of using the range function"""
-
-# The will print a list of the first 100 numbers
-
-print range(100)
--- 12c.SortaRandom.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-"""Will print a single random number between 0-99"""
-
-# Let's use someone elses code
-import random
-
-
-print random.randrange(100)
